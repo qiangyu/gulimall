d108ef023c14cd70022c5a3f1bf2a15d72e82a83
qiangyu <823631154@qq.com>
2022-10-04 23:07:22 +0800

    1. add getaway and third-part pom 2. 完成商品属性分组功能

---
+++ gulimall-third-part/src/main/java/cn/zqyu/gulimall/thirdpart/GulimallThirdPartApplication.java
@@ -0,0 +1,13 @@
+package cn.zqyu.gulimall.thirdpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallThirdPartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallThirdPartApplication.class, args);
+    }
+
+}
---
+++ gulimall-third-part/src/test/java/cn/zqyu/gulimall/thirdpart/GulimallThirdPartApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zqyu.gulimall.thirdpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallThirdPartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
